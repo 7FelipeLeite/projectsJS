88059bab3ef1ea4a9d6c79e15f687e31e948d5bd
Felipe <felipeleitectt@gmail.com>
2026-04-10 15:39:39 -0300

    Começando a implementar as tasks como objeto para controlar as tasks em POO

--- toDo/js/main.js
+++ toDo/js/main.js
@@ -1,18 +1,19 @@
-let tasks = [{ id: 1, text: "Estudar JS", done: false }];
+let tasks = [];
+let idCounter = 1;
 
 //DOM
 const inputTxtTask = document.getElementById("inputTxtTask");
 const taskButton = document.getElementById("taskButton");
 const taskOrderedList = document.getElementById("taskList");
 
-function createTaskElement(text) {
+function createTaskElement(task) {
   const li = document.createElement("li");
   const span = document.createElement("span");
   const checkButton = document.createElement("button");
-  const eraseButton = document.createElement("button");
-
   checkButton.textContent = "✅";
+  const eraseButton = document.createElement("button");
   eraseButton.textContent = "🗑️";
+
   span.innerText = text;
   li.appendChild(span);
   li.appendChild(checkButton);
@@ -24,15 +25,28 @@ function createTaskElement(text) {
 //LÓGICA
 taskButton.addEventListener("click", () => {
   if (inputTxtTask.value.trim() != "") {
-    const task = createTaskElement(inputTxtTask.value);
-    taskOrderedList.appendChild(task);
+    //AO CLICAR NO BOTÃO CRIA UM OBJETO
+    const task = {
+      id: idCounter++,
+      text: inputTxtTask.value,
+      done: false,
+    };
+    //COLOCA ESSE OBJETO DENTRO DO ARRAY TASK
+    tasks.push(task);
+    console.log(tasks);
+
+    //CRIA A VARIAVEL taskElement E COLOCA TUDO ISSO DENTRO DO OL
+    const taskElement = createTaskElement(task);
+    taskOrderedList.appendChild(taskElement);
+
+    //Zera o input
     inputTxtTask.value = "";
-    inputTxtTask.placeholder = "Digite uma tarefa";
   } else {
     inputTxtTask.placeholder = "Digite uma tarefa valida";
   }
 });
 
+//Função nativa pro enter clicar no botão enviar
 inputTxtTask.addEventListener("keydown", (event) => {
   if (event.key === "Enter") {
     event.preventDefault();
